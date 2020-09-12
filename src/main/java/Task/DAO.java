eda0adb5bbfbdc1d393a0322e0775775ea870849
IgorBoykov <62951857+IgorBoykov@users.noreply.github.com>
2020-09-12 21:09:25 +0300

    Update DAO.java

--- src/main/java/Task/DAO.java
+++ src/main/java/Task/DAO.java
@@ -12,6 +12,8 @@ public class DAO {
 	public static String READ_ALL = "select * from student";
 	public static String CREATE = "insert into student(name, age) values(?,?)";
 	public static String DELETE = "delete from student where id =?";
+	private static String READ_ID = "select * from student where id =?";
+	private static String UPDATE = "update student set name=?, age = ? where id = ?";
 
 	private Connection connection;
 	private PreparedStatement preparedStatement;
@@ -40,6 +42,22 @@ public class DAO {
 	public void delete(int id) throws SQLException {
 		preparedStatement = connection.prepareStatement(DELETE);
 		preparedStatement.setInt(1, id);
+		preparedStatement.executeUpdate();
+	}
+	
+	public Student read_id(int id) throws SQLException {
+		preparedStatement = connection.prepareStatement(READ_ID);
+		preparedStatement.setInt(1, id);
+		ResultSet result = preparedStatement.executeQuery();
+		result.next();
+		return Mapper.showStudent(result);
+	}
+
+	public void update(Student student) throws SQLException {
+		preparedStatement = connection.prepareStatement(UPDATE);
+		preparedStatement.setString(1, student.getName());
+		preparedStatement.setInt(2, student.getAge());
+		preparedStatement.setInt(3, student.getId());
 		preparedStatement.executeUpdate();
 	}
 
